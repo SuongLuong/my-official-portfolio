835f2b265d5a2b2c9fdd0d9ad398ea617fb2c57b
Kingsley Luong <ducsuongluong@gmail.com>
2019-12-19 15:08:54 -0500

    add about and contact section

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,9 +20,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `Kingsley Luong - Portfolio`,
+        short_name: `KL`,
+        description: `Personal`,
         start_url: `/`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,
